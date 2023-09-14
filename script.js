75b43ad48407f49d75fdacb027f8e52b4bf292aa
babyo7 <144552425+babyo77@users.noreply.github.com>
2023-09-15 04:18:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -610,6 +610,9 @@ finMinMax()
 
 
 price.oninput = (e)=> {console.log(price.value)
+    if (price.value==0){
+        gta(data)
+    }
   pricetext.innerText= "₹ " + e.target.value
   load.style.display="none"
     gta(data.filter((product) => product.ammount <= e.target.value));
