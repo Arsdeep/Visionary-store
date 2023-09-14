551e1de3f40d99134548f8d1b6cea9b035be34ff
babyo7 <144552425+babyo77@users.noreply.github.com>
2023-09-15 04:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -610,7 +610,7 @@ finMinMax()
 
 
 price.oninput = (e)=> {console.log(price.value)
-    if (pricetext.innerText==0){
+    if (price.value=0){
         gta(data)
     }else{
   pricetext.innerText= "₹ " + e.target.value
